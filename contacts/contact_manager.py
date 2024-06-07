405a93cc938e9820fb73f94993dbd028ff967ba9
nksn7 <niksonpoke@gmail.com>
2024-06-07 12:54:35 -0300

    Criando funcao adicionar_contato

--- contacts/contact_manager.py
+++ contacts/contact_manager.py
@@ -8,3 +8,16 @@ def listar_contatos(lista):
         string_listagem += f"{contato}\n"
 
     return string_listagem
+
+
+def adicionar_contato(id_contato, nome, telefone, email):
+    contato = {"ID": id_contato, "Nome": nome, "Telefone": telefone, "Email": email}
+
+    for elemento in contatos:
+
+        if elemento == contato:
+            return False
+
+    contatos.append(contato)
+
+    return True
